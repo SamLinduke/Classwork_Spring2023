839ab026f44287815960d463b358a3e7cc707ff3
Sam Lin <zl361@duke.edu>
2023-01-20 12:38:56 -0500

    add blood calculator function quit while 9

---
+++ blood_calculator.py
@@ -0,0 +1,10 @@
+def interface():
+    print("blood calculator")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Select an options:")
+    if choice == "9":
+        return
+        
+
+interface()
